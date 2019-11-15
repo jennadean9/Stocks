0adf43f96b3059ffa3e24cf9efc860a180e788b6
Jenna Dean <jenna.dean9@gmail.com>
2019-11-15 08:39:28 -0500

    init

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(require("./routes/stock.js"));
+
+app.listen(4001, () => console.log("listening on port 4001"));
