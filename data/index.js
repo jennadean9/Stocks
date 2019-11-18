c1837714e2201b2f9183614ed6269d5c8dca41de
Jenna Dean <jenna.dean9@gmail.com>
2019-11-18 06:22:05 -0500

    cors

--- data/index.js
+++ data/index.js
@@ -4,15 +4,6 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 app.use(require("./routes/route.js"));
 
 app.listen(4001, () => console.log("listening on port 4001"));
